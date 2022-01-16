815dfaf586850d06e4cb90b1bcfc54a90b445853
Сергей Выдрин <sssssssss062@gmail.com>
2022-01-16 19:21:52 +0400

    1

--- voprosy/Form1.Designer.cs
+++ voprosy/Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace voprosy
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,7 +49,8 @@ namespace voprosy
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
-            this.Column3});
+            this.Column3,
+            this.Column4});
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1369, 410);
@@ -60,33 +63,51 @@ namespace voprosy
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Import excel";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Column1
             // 
-            this.Column1.FillWeight = 191.8896F;
+            this.Column1.FillWeight = 216.8905F;
             this.Column1.HeaderText = "Вопрос";
             this.Column1.Name = "Column1";
             // 
             // Column2
             // 
-            this.Column2.FillWeight = 39.58239F;
+            this.Column2.FillWeight = 44.73949F;
             this.Column2.HeaderText = "Ответ";
             this.Column2.Name = "Column2";
             // 
             // Column3
             // 
-            this.Column3.FillWeight = 68.5279F;
+            this.Column3.FillWeight = 77.45625F;
             this.Column3.HeaderText = "Признак";
             this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 60.9137F;
+            this.Column4.HeaderText = "Номер";
+            this.Column4.Name = "Column4";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1225, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Upd";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1393, 458);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -104,6 +125,8 @@ namespace voprosy
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- voprosy/Form1.cs
+++ voprosy/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,6 +14,7 @@ namespace voprosy
 {
     public partial class Form1 : Form
     {
+        public static List<string> arr = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -22,20 +24,80 @@ namespace voprosy
         {
             StreamReader f = new StreamReader("вопросы1.txt");
             int i = 0;
-            while (!f.EndOfStream)
+            int number = 0;
+            string vopros = "";
+            string otvet = "";
+            while (!f.EndOfStream) //добавляем строки в массив
             {
-                string s = f.ReadLine();
-                //MessageBox.Show(s);
-                dataGridView1.Rows.Add();
-                i++;
-                dataGridView1[0, i-1].Value = s;
+                arr.Add(f.ReadLine());
             }
             f.Close();
+
+            for (int z = 0; z < arr.Count - 2; z++)
+            {
+                if (arr[z].Contains("Вопрос"))//Ищем строку с текстом вопрос
+                {
+                    z++;
+                    //MessageBox.Show(arr[z]);
+                    vopros = arr[z];
+                    number++;
+                    //z++; z++; z++;
+
+                    while (arr[z].StartsWith("N")) //пролистываем строки до вариантов ответа
+                    { 
+                        z++; 
+                    }
+                    z++;
+                    while (arr[z] != "")
+                    {
+                        //MessageBox.Show(vopros + "////" + arr[z]);
+                        otvet = arr[z];
+                        //if (arr[z+1].StartsWith("-"))
+                        //{
+                        //    
+                        //}
+                        dataGridView1.Rows.Add();
+                        dataGridView1[0, i].Value = vopros;
+                        dataGridView1[1, i].Value = otvet;
+                        dataGridView1[3, i].Value = number.ToString();
+                        i++;
+                        z++;
+                    }
+                }
+            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string filePath = (Application.StartupPath) + @"otvet.xlsx";
+            XLWorkbook xLWorkbook = new XLWorkbook();
+            var excelworksheet = xLWorkbook.Worksheets.Add(1);
+            for (int z = 0; z < dataGridView1.RowCount - 2; z++)
+            {
+                excelworksheet.Cell(z+1,1).Value = dataGridView1[0, z].Value.ToString();
+                excelworksheet.Cell(z+1,2).Value = dataGridView1[1, z].Value.ToString();
+                excelworksheet.Cell(z+1,3).Value = dataGridView1[2, z].Value.ToString();
+            }
+            xLWorkbook.SaveAs((Application.StartupPath) + @"\otvet.xlsx");
+            MessageBox.Show((Application.StartupPath) + @"\otvet.xlsx");
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int z = 0; z < dataGridView1.RowCount - 2; z++)
+            {
+                dataGridView1[1, z].Value = dataGridView1[1, z].Value.ToString().Replace("1.", "").Replace("2.", "").Replace("3.", "").Replace("4.", "");
+                if (dataGridView1[1, z].Value.ToString().Contains("<@1>") && dataGridView1[1, z].Value != null)
+                {
+                    dataGridView1[1, z].Value = dataGridView1[1, z].Value.ToString().Replace("<@1>", "");
+                    dataGridView1[2, z].Value = "true";
+                }
+                else
+                {
+                    dataGridView1[2, z].Value = "false";
+                }
+            }
         }
     }
 }
